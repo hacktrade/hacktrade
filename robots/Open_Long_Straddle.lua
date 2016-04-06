8748e4da69e946b1c25c5c3701fd824e69306f91
gihar <a.timchenko@gmail.com>
2016-04-06 15:05:52 +0300

    изменен расчет теоретической цены

--- robots/Open_Long_Straddle.lua
+++ robots/Open_Long_Straddle.lua
@@ -17,10 +17,10 @@ function Robot()
 	local MAX_OPT_QTY = 4			-- максимальное количество опционов для покупки
 	local OPT_LOT = 2				-- количество лотов для заявки на покупку опционов
 	local MAX_LAG = 2				-- максимальное превышение цены над теоретической ценой (указывается в ШАГАХ ЦЕНЫ)
-	local MAX_VOLA = 35.5			-- максимальная волатильность опционов, по которой мы готовы покупать, указывается в процентах
+	local MAX_VOLA = 36				-- максимальная волатильность опционов, по которой мы готовы покупать, указывается в процентах
 	local FIX_VOLA = 35 			-- желательная волатильность, указывается в процентах
     local CONST = 1					-- отступ от лучшей цены для заявок (указывается в ШАГАХ ЦЕНЫ)
-	local COMMENT = "Straddle"		-- комментарий к заявке для "Истории позиций"
+	local COMMENT = "Str"			-- комментарий к заявке для "Истории позиций"
 
 	local SLEEP_WITH_ORDER = 60000	-- время ожидания исполнения выставленного ордера до пересчета теоретичской цены (в миллисекундах)
 	local SLEEP_WO_ORDER = 100		-- время ожидания после снятия ордера (в миллисекундах)
@@ -30,7 +30,8 @@ function Robot()
 	local working = true
     local vola
     local best_offer
-    local theor_price
+    local theor_price_calc
+	local theor_price_fix
     local theor_price_quik
     local opt_qty = 0				-- начальная позиция по опционам
 
@@ -60,11 +61,11 @@ function Robot()
 
     local optionbase=getParamEx(OPT_CLASS,OPT_TICKER,"optionbase").param_image
     local optiontype=getParamEx(OPT_CLASS,OPT_TICKER,"optiontype").param_image
-    
+
     --local step=getParamEx(OPT_CLASS,OPT_TICKER,"SEC_PRICE_STEP,").param_value
     local step=opt_feed.SEC_PRICE_STEP
 	--log:debug("step="..step)
-	
+
     log:debug("optionbase= "..optionbase.." optiontype= "..optiontype)
     log:debug("MAX_VOLA="..MAX_VOLA.." FIX_VOLA="..FIX_VOLA)
 
@@ -82,7 +83,7 @@ function Robot()
         repeat
 
 			i = i + 1
-	    	vola = opt_feed.volatility                -- текущая волатильность опциона
+	    	vola = opt_feed.volatility               	-- текущая волатильность опциона из QUIK
 
             local tmpParam = {
                     ["optiontype"] = optiontype,                                                                -- тип опциона
@@ -92,37 +93,49 @@ function Robot()
                     ["DAYS_TO_MAT_DATE"] = getParamEx(OPT_CLASS,OPT_TICKER,"DAYS_TO_MAT_DATE").param_value+0    -- число дней до экспирации опциона
             }
 
-            theor_price = TheorPrice (tmpParam)                                                         -- наша теоретическая цена
-            theor_price = theor_price - math.fmod(theor_price,step) 									-- округляем до шага цены вниз
+            theor_price_fix = TheorPrice (tmpParam)                                                         	-- наша теоретическая цена
+            theor_price_fix = theor_price_fix - math.fmod(theor_price_fix,step) 								-- округляем до шага цены вниз
+
+            local tmpParam = {
+                    ["optiontype"] = optiontype,                                                                -- тип опциона
+                    ["settleprice"] = getParamEx(FUT_CLASS,FUT_TICKER,"settleprice").param_value+0,             -- текущая цена фьючерса
+                    ["strike"] = getParamEx(OPT_CLASS,OPT_TICKER,"strike").param_value+0,                       -- страйк опциона
+                    ["volatility"] = vola, 													                    -- волатильность опциона: берём волатильность из QUIK
+                    ["DAYS_TO_MAT_DATE"] = getParamEx(OPT_CLASS,OPT_TICKER,"DAYS_TO_MAT_DATE").param_value+0    -- число дней до экспирации опциона
+            }
+
+            theor_price_calc = TheorPrice (tmpParam)                                                         	-- наша теоретическая цена
+            theor_price_calc = theor_price_calc - math.fmod(theor_price_calc,step) 								-- округляем до шага цены вниз
+
 
 		    best_offer = opt_feed.offers[1].price --потом убрать, нужно для лога
 
 		    log:debug ("==============================================================================================#"..i.."=")
-			log:debug ("vola="..vola.." max_vola="..MAX_VOLA.." best_offer="..best_offer.." theor_price="..theor_price.." quik_theor_price="..opt_feed.theorprice.." opt_qty="..opt_qty)
+			log:debug ("vola="..vola.." max_vola="..MAX_VOLA.." best_offer="..best_offer.." theor_price_fix="..theor_price_fix.." theor_price_calc="..theor_price_calc.." quik_theor_price="..opt_feed.theorprice.." opt_qty="..opt_qty)
 
-		    if vola > MAX_VOLA then                                  -- если текущая волатильность слишком высокая, то используем теоретическую цену, рассчитанную
-		    	                                                     -- на базе желаемой волатильности по формуле Блека-Шоулза
-                opt_order:update(theor_price, opt_qty)               -- выставляем заявку по нашей теоретической цене
-                log:debug("decision:1 >> theor_price")
+		    if vola > MAX_VOLA then                                  	-- если текущая волатильность слишком высокая, то используем теоретическую цену, рассчитанную
+																		-- на базе желаемой волатильности по формуле Блека-Шоулза
+                opt_order:update(theor_price_fix, opt_qty)            	-- выставляем заявку по нашей теоретической цене
+                log:debug("decision:1 >> theor_price_fix")
 		    else
-		    	best_offer = opt_feed.offers[1].price   			 -- минимальная цена предложения BEST_OFFER
+		    	best_offer = opt_feed.offers[1].price   			 	-- минимальная цена предложения BEST_OFFER
 
-                if best_offer < theor_price then
+                if best_offer < theor_price_calc then
 
                     opt_order:update(best_offer + CONST * step, opt_qty)
                     log:debug("decision:2 >> best_offer + CONST")
 
-                elseif best_offer == theor_price then
+                elseif best_offer == theor_price_calc then
 
                     opt_order:update(best_offer, opt_qty)
                     log:debug("decision:3 >> best_offer")
 
-                elseif best_offer > theor_price then
+                elseif best_offer > theor_price_calc then
 
-                    if best_offer > theor_price + MAX_LAG * step then
+                    if best_offer > theor_price_calc + MAX_LAG * step then
 
-                        opt_order:update(theor_price, opt_qty)
-                        log:debug("decision:4 >> theor_price")
+                        opt_order:update(theor_price_calc, opt_qty)
+                        log:debug("decision:4 >> theor_price_calc")
 
                     else
                         opt_order:update(best_offer - CONST * step, opt_qty)
